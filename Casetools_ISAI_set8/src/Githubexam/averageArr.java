c67a64cc8cb3e6b8705491fcfe5ac456703b6f54
exam <rbca22131.rcca@gmail.com>
2024-02-12 09:41:43 +0530

    commit

---
+++ Casetools_ISAI_set8/src/Githubexam/averageArr.java
@@ -0,0 +1,14 @@
+package Githubexam;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class averageArr {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
